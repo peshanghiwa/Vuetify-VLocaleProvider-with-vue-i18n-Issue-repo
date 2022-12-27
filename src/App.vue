dd45151dcd6709b8a5203f03c3c424f7803624b9
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2022-12-27 13:56:12 +0300

    issue reproducer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <!-- <v-locale-provider rtl> -->
-  <v-btn color="success"> change direction </v-btn>
-  <!-- </v-locale-provider> -->
+  <v-locale-provider rtl>
+    <v-btn color="success"> change direction </v-btn>
+  </v-locale-provider>
 </template>
 
 <style scoped></style>
